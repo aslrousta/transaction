13437618e57e19fe71f8b5a7c8e8a5f75d4fe44b
Ali AslRousta <ali.aslrousta@snapp.cab>
2017-11-06 17:11:33 +0330

    Transfer API implemented

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\Transaction\InsufficientBalanceException;
+use App\Services\Transaction\InvalidAccountException;
+use App\Services\Transaction\TransactionManagerInterface;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Validator;
 
 class TransactionController extends Controller
@@ -11,6 +16,7 @@ class TransactionController extends Controller
      * Transfers currency between two accounts.
      *
      * @param Request $request
+     * @return JsonResponse
      */
     public function transfer(Request $request)
     {
@@ -19,8 +25,23 @@ class TransactionController extends Controller
             'to'     => 'required|integer',
             'amount' => 'required|integer',
         ]);
-
         $validator->validate();
-        $data = $validator->getData();
+
+        $data   = $validator->getData();
+        $from   = $data['from'];
+        $to     = $data['to'];
+        $amount = $data['amount'];
+
+        /** @var TransactionManagerInterface $transactionManager */
+        $transactionManager = app(TransactionManagerInterface::class);
+
+        try {
+            $balance = $transactionManager->transfer($from, $to, $amount);
+            return Response::json(['balance' => $balance]);
+        } catch (InvalidAccountException $e) {
+            return Response::json(['error' => 'Account Not Found'], 404);
+        } catch (InsufficientBalanceException $e) {
+            return Response::json(['error' => 'Insufficient Balance'], 406);
+        }
     }
 }
